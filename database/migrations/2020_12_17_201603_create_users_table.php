d1ef1f9052a5a99fac16f70cfeddedbbded0ee52
Shahid Karimi <karimi.shahid@gmail.com>
2020-12-18 01:32:54 +0500

    seeders

--- database/migrations/2020_12_17_201603_create_users_table.php
+++ database/migrations/2020_12_17_201603_create_users_table.php
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        //create_users_table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('firstname');
+            $table->string('lastname')->nullable();
             $table->timestamps();
         });
     }
