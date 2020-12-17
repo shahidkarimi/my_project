107c271f0cfe313b19a7bd575c3029d506811f0b
Shahid Karimi <karimi.shahid@gmail.com>
2020-12-18 01:05:10 +0500

    database configurations

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('my-test', function(){
+   $name = "My Test";
+   return View('my-test',['name' => $name]);
+});
