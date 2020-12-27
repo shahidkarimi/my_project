e9c1667a450bc3c88515fbb97f1827e0f8ee67c7
Shahid Karimi <karimi.shahid@gmail.com>
2020-12-28 02:01:52 +0500

    updates

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>@yield('title','Home')</title>
+</head>
+
+<body>
+    <div>
+        <a href="/">Home</a>
+        |
+        <a href="/pages/about">About</a>
+        |
+        <a href="/pages/contact">Contact</a>
+    </div>
+
+    <div>
+        @yield('content')
+    </div>
+
+    @yield('end-js')
+</body>
+
+</html>
